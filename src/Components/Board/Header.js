4a01b329ef2f90b48314417874145fd52eb7e99a
unknown <aviad.bourla@gmail.com>
2020-10-18 11:17:57 +0300

    header

--- src/Components/Board/Header.js
+++ src/Components/Board/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = () => {
     return (
         <div>
-            <h1 > iBourla
+            <h1 > Farkle
         <i style={{ paddingLeft: '1rem' }} className="fas fa-dice-six"> </i>
             </h1>
         </div>
